356970157acea406cad9fee9538b6c3cc3cc1103
mahdiridho <mahdi.ridho@gmail.com>
2021-09-29 22:12:46 +1000

    shopapp

---
+++ shop-app/rollup.config.js
@@ -0,0 +1,55 @@
+import merge from 'deepmerge';
+// use createSpaConfig for bundling a Single Page App
+import { createSpaConfig } from '@open-wc/building-rollup';
+import { injectManifest } from 'rollup-plugin-workbox';
+import copy from 'rollup-plugin-copy';
+
+const outputDir = 'build';
+
+// use createBasicConfig to do regular JS to JS bundling
+// import { createBasicConfig } from '@open-wc/building-rollup';
+
+const baseConfig = createSpaConfig({
+  // use the outputdir option to modify where files are output
+  outputDir: outputDir,
+
+  // if you need to support older browsers, such as IE11, set the legacyBuild
+  // option to generate an additional build just for this browser
+  // legacyBuild: true,
+
+  // development mode creates a non-minified build for debugging or development
+  developmentMode: process.env.ROLLUP_WATCH === 'true',
+
+  // set to true to inject the service worker registration into your index.html
+  injectServiceWorker: false,
+  workbox: false
+});
+
+export default merge(baseConfig, {
+  // if you use createSpaConfig, you can use your index.html as entrypoint,
+  // any <script type="module"> inside will be bundled by rollup
+  input: './index.html',
+
+  // alternatively, you can use your JS as entrypoint for rollup and
+  // optionally set a HTML template manually
+  // input: './app.js',
+
+  plugins: [
+    copy({
+      targets: [
+        { src: 'images/*', dest: outputDir+'/images' },
+        { src: 'data/*', dest: outputDir+'/data' },
+        { src: 'manifest.json', dest: outputDir }
+      ],
+      // set flatten to false to preserve folder structure
+      flatten: true,
+    }),
+    injectManifest({
+      swSrc: 'pwabuilder-sw.js',
+      swDest: outputDir +'/pwabuilder-sw.js',
+      globPatterns: ['index.html','manifest.json','src/**/*.{json,js}','data/**/*.{json,jpg,png},'images/**/*.{ico,jpg,png}'],
+      globDirectory: outputDir +'/',
+      maximumFileSizeToCacheInBytes: 4 * 1024 * 1024
+    })
+  ],
+});
